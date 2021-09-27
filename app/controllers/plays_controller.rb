f5d03039874b156b257a494a756cff35e999e8b7
Tasso Rodrigues <tasso_henrique@hotmail.com>
2021-09-27 18:39:59 -0400

    plays controllers

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,2 +1,40 @@
 class PlaysController < ApplicationController
+  def index
+    @plays = Play.all
+  end
+
+  def show
+    @play = Play.find(params[:id])
+  end
+
+  def new
+    @play = Play.new
+  end
+
+  def edit
+    @play = Play.find(params[:id])
+  end
+
+  def update
+    @play = Play.find(params[:id])
+    if @play.update(play_params)
+      redirect_to @play
+    else
+      render :edit
+  end
+
+  def create
+    @play = Play.new(play_params)
+    if @play.save
+      redirect_to plays_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def play_params
+    params.require(:play).permit(:game, :category, :video, :title, :comment, :nickname)
+  end
 end
