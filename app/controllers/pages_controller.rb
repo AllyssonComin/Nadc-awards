5f36cc47bb5859cad7737eecc5f474f1839a281e
Tasso Rodrigues <tasso_henrique@hotmail.com>
2022-02-16 13:38:07 -0400

    days counter and fixed some css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def home    
-  end
+  # def countdown
+  #   start_date = Date.now
+  #   end_date = Date.parse "2021-04-02 14:46:21 +0200"
+
+  # end
 end
